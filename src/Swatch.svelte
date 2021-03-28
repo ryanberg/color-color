1d0945a60cc28493e2e8d721604d35dbff559278
Ryan Berg <ryan.berg@gmail.com>
2021-03-28 01:25:21 -0400

    Fix broken overlay checkboxes

--- src/Swatch.svelte
+++ src/Swatch.svelte
@@ -78,11 +78,12 @@
 
   //$: isLight = chroma(hexCode).luminance() > 0.55;
 
-  $: textColor = isLight ? textColors.dark : textColors.light;
-  $: whiteContrast =
-    $settings.overlayContrast && chroma.contrast("#fff", hexCode);
-  $: blackContrast =
-    $settings.overlayContrast && chroma.contrast("#000", hexCode);
+  // $: whiteContrast =
+  //   $settings.overlayContrast && chroma.contrast("#fff", hexCode);
+  // $: blackContrast =
+  //   $settings.overlayContrast && chroma.contrast("#000", hexCode);
+  $: whiteContrast = chroma.contrast("#fff", hexCode);
+  $: blackContrast = chroma.contrast("#000", hexCode);
   $: refColor = $nearestRefColors[hexCode];
 
   $: whiteContrastAa = whiteContrast >= 4.5;
@@ -91,6 +92,8 @@
   $: blackContrastAaLarge = blackContrast >= 3;
 
   $: isLight = blackContrastAa;
+
+  $: textColor = isLight ? textColors.dark : textColors.light;
 </script>
 
 <div
@@ -104,26 +107,35 @@
   <div class="flex w-full my-auto pl-3">
     <div class="mr-auto pr-2 xl:pr-3" style="color: {textColor}">
       <div class="text-lg sm:text-xl leading-none font-bold mb-1">{stepId}</div>
-      <div class="text-sm leading-none font-mono">{hexCode}</div>
+      {#if $settings.overlayHex}
+        <div class="text-sm leading-none font-mono">{hexCode}</div>
+      {/if}
     </div>
 
-    <div class="w-15 xl:w-16 pr-2 xl:pr-3 flex-grow-0 flex-shrink-0 text-white">
-      <div class="w-full text-lg sm:text-xl leading-none font-bold mb-1">
-        {#if whiteContrastAaLarge}AA+{/if}
+    {#if $settings.overlayContrast}
+      <div
+        class="w-15 xl:w-16 pr-2 xl:pr-3 flex-grow-0 flex-shrink-0 text-white">
+        <div class="w-full text-lg sm:text-xl leading-none font-bold mb-1">
+          {#if whiteContrastAaLarge}AA+{/if}
+        </div>
+        <div class="w-full text-sm leading-none">
+          {#if whiteContrastAa}AA{/if}
+        </div>
       </div>
-      <div class="w-full text-sm leading-none">
-        {#if whiteContrastAa}AA{/if}
-      </div>
-    </div>
+    {/if}
 
-    <div class="w-15 xl:w-16 pr-2 xl:pr-3 flex-grow-0 flex-shrink-0 text-black">
-      <div class="w-full text-lg sm:text-xl leading-none font-bold mb-1">
-        {#if blackContrastAaLarge}AA+{/if}
+    {#if $settings.overlayContrast}
+      <div
+        class="w-15 xl:w-16 pr-2 xl:pr-3 flex-grow-0 flex-shrink-0 text-black">
+        <div class="w-full text-lg sm:text-xl leading-none font-bold mb-1">
+          {#if blackContrastAaLarge}AA+{/if}
+        </div>
+        <div class="w-full text-sm leading-none">
+          {#if blackContrastAa}AA{/if}
+        </div>
       </div>
-      <div class="w-full text-sm leading-none">
-        {#if blackContrastAa}AA{/if}
-      </div>
-    </div>
+    {/if}
+
     {#if false}
       <span class=""></span>
       {#if $settings.overlayHex}<span class="hex-code"></span>{/if}
